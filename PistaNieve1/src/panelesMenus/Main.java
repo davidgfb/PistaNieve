4eae67661ca5ff3e874689ff11ab7d0a2cea1658
davidgfb <davidgracia99@gmail.com>
2020-10-29 17:13:40 +0100

    1

---
+++ PistaNieve1/src/panelesMenus/Main.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package panelesMenus;
+
+//<editor-fold defaultstate="collapsed" desc="imports">
+import static java.awt.Color.white;
+import javax.swing.JFrame;
+import javax.swing.Timer;
+//</editor-fold>
+
+/**
+ *
+ * @author David
+ */
+public class Main {
+    
+    //<editor-fold defaultstate="collapsed" desc="main">
+    public static void main(String[] args) {
+        JFrame f = new JFrame();
+        PMenu pM = new PMenu();
+        Timer t = new Timer(1000,pM);
+        t.start();
+        
+        f.setDefaultCloseOperation(3);
+        f.setSize(800, 600);
+        pM.setBackground(white);
+        f.add(pM);
+        f.setVisible(true);
+    }
+//</editor-fold>
+}

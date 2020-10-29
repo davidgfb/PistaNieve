b74c711fb41a76d82229c67bd0cc357d9afe2e95
davidgfb <davidgracia99@gmail.com>
2020-10-29 13:45:39 +0100

    1

--- PistaNieve1/src/modelo/sinUso/Nivel.java
+++ PistaNieve1/src/modelo/sinUso/Nivel.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package modelo;
+package modelo.sinUso;
 
 /**
  *
  * @author David
  */
-public class Mapa {
+public class Nivel {
     
 }
